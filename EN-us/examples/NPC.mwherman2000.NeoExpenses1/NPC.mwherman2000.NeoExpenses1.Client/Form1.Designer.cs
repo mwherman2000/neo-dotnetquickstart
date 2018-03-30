d2c95c24c515714dc7fd4aaa1e168bf0c7962bb4
Michael Herman (Toronto) <mwherman@parallelspace.net>
2018-03-30 07:46:33 -0400

    Updates

---
+++ EN-us/examples/NPC.mwherman2000.NeoExpenses1/NPC.mwherman2000.NeoExpenses1.Client/Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace NPC.mwherman2000.NeoExpenses1.Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonEncryptFile = new System.Windows.Forms.Button();
+            this.buttonDecryptFile = new System.Windows.Forms.Button();
+            this.buttonCreateAsmKeys = new System.Windows.Forms.Button();
+            this.buttonExportPublicKey = new System.Windows.Forms.Button();
+            this.buttonImportPublicKey = new System.Windows.Forms.Button();
+            this.buttonGetPrivateKey = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBoxExportPrivateKey = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textUsername = new System.Windows.Forms.TextBox();
+            this.maskedPassword = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonCreateRequisition = new System.Windows.Forms.Button();
+            this.buttonSubmitRequisition = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textApprover = new System.Windows.Forms.TextBox();
+            this.comboRole = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonEncryptFile
+            // 
+            this.buttonEncryptFile.Location = new System.Drawing.Point(21, 116);
+            this.buttonEncryptFile.Name = "buttonEncryptFile";
+            this.buttonEncryptFile.Size = new System.Drawing.Size(146, 37);
+            this.buttonEncryptFile.TabIndex = 0;
+            this.buttonEncryptFile.Text = "Encrypt File";
+            this.buttonEncryptFile.UseVisualStyleBackColor = true;
+            this.buttonEncryptFile.Click += new System.EventHandler(this.buttonEncryptFile_Click);
+            // 
+            // buttonDecryptFile
+            // 
+            this.buttonDecryptFile.Location = new System.Drawing.Point(50, 111);
+            this.buttonDecryptFile.Name = "buttonDecryptFile";
+            this.buttonDecryptFile.Size = new System.Drawing.Size(146, 42);
+            this.buttonDecryptFile.TabIndex = 1;
+            this.buttonDecryptFile.Text = "Decrypt File";
+            this.buttonDecryptFile.UseVisualStyleBackColor = true;
+            this.buttonDecryptFile.Click += new System.EventHandler(this.buttonDecryptFile_Click);
+            // 
+            // buttonCreateAsmKeys
+            // 
+            this.buttonCreateAsmKeys.Location = new System.Drawing.Point(21, 30);
+            this.buttonCreateAsmKeys.Name = "buttonCreateAsmKeys";
+            this.buttonCreateAsmKeys.Size = new System.Drawing.Size(146, 37);
+            this.buttonCreateAsmKeys.TabIndex = 2;
+            this.buttonCreateAsmKeys.Text = "Create Asm Keys";
+            this.buttonCreateAsmKeys.UseVisualStyleBackColor = true;
+            this.buttonCreateAsmKeys.Click += new System.EventHandler(this.buttonCreateAsmKeys_Click);
+            // 
+            // buttonExportPublicKey
+            // 
+            this.buttonExportPublicKey.Location = new System.Drawing.Point(21, 73);
+            this.buttonExportPublicKey.Name = "buttonExportPublicKey";
+            this.buttonExportPublicKey.Size = new System.Drawing.Size(146, 37);
+            this.buttonExportPublicKey.TabIndex = 3;
+            this.buttonExportPublicKey.Text = "Export Public Key";
+            this.buttonExportPublicKey.UseVisualStyleBackColor = true;
+            this.buttonExportPublicKey.Click += new System.EventHandler(this.buttonExportPublicKey_Click);
+            // 
+            // buttonImportPublicKey
+            // 
+            this.buttonImportPublicKey.Location = new System.Drawing.Point(187, 116);
+            this.buttonImportPublicKey.Name = "buttonImportPublicKey";
+            this.buttonImportPublicKey.Size = new System.Drawing.Size(146, 37);
+            this.buttonImportPublicKey.TabIndex = 4;
+            this.buttonImportPublicKey.Text = "Import Public Keys";
+            this.buttonImportPublicKey.UseVisualStyleBackColor = true;
+            this.buttonImportPublicKey.Click += new System.EventHandler(this.buttonImportPublicKey_Click);
+            // 
+            // buttonGetPrivateKey
+            // 
+            this.buttonGetPrivateKey.Location = new System.Drawing.Point(350, 116);
+            this.buttonGetPrivateKey.Name = "buttonGetPrivateKey";
+            this.buttonGetPrivateKey.Size = new System.Drawing.Size(146, 37);
+            this.buttonGetPrivateKey.TabIndex = 5;
+            this.buttonGetPrivateKey.Text = "Get Private Key";
+            this.buttonGetPrivateKey.UseVisualStyleBackColor = true;
+            this.buttonGetPrivateKey.Click += new System.EventHandler(this.buttonGetPrivateKey_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(184, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "--";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBoxExportPrivateKey);
+            this.groupBox1.Controls.Add(this.buttonExportPublicKey);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.buttonCreateAsmKeys);
+            this.groupBox1.Controls.Add(this.buttonGetPrivateKey);
+            this.groupBox1.Controls.Add(this.buttonEncryptFile);
+            this.groupBox1.Controls.Add(this.buttonImportPublicKey);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(566, 177);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Current Key Management";
+            // 
+            // checkBoxExportPrivateKey
+            // 
+            this.checkBoxExportPrivateKey.AutoSize = true;
+            this.checkBoxExportPrivateKey.Checked = true;
+            this.checkBoxExportPrivateKey.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxExportPrivateKey.Location = new System.Drawing.Point(187, 82);
+            this.checkBoxExportPrivateKey.Name = "checkBoxExportPrivateKey";
+            this.checkBoxExportPrivateKey.Size = new System.Drawing.Size(146, 21);
+            this.checkBoxExportPrivateKey.TabIndex = 7;
+            this.checkBoxExportPrivateKey.Text = "Export Private Key";
+            this.checkBoxExportPrivateKey.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonDecryptFile);
+            this.groupBox2.Location = new System.Drawing.Point(599, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(246, 177);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Decrypt with Current Key";
+            // 
+            // textUsername
+            // 
+            this.textUsername.Location = new System.Drawing.Point(263, 31);
+            this.textUsername.Name = "textUsername";
+            this.textUsername.Size = new System.Drawing.Size(233, 22);
+            this.textUsername.TabIndex = 10;
+            // 
+            // maskedPassword
+            // 
+            this.maskedPassword.Location = new System.Drawing.Point(263, 70);
+            this.maskedPassword.Name = "maskedPassword";
+            this.maskedPassword.Size = new System.Drawing.Size(233, 22);
+            this.maskedPassword.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(184, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(184, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Password";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dataGridView1);
+            this.groupBox3.Controls.Add(this.comboRole);
+            this.groupBox3.Controls.Add(this.textApprover);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.buttonSubmitRequisition);
+            this.groupBox3.Controls.Add(this.buttonCreateRequisition);
+            this.groupBox3.Controls.Add(this.buttonLogin);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.textUsername);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.maskedPassword);
+            this.groupBox3.Location = new System.Drawing.Point(12, 210);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(833, 412);
+            this.groupBox3.TabIndex = 14;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "NeoExpenses App";
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(21, 31);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(146, 37);
+            this.buttonLogin.TabIndex = 14;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonCreateRequisition
+            // 
+            this.buttonCreateRequisition.Location = new System.Drawing.Point(187, 106);
+            this.buttonCreateRequisition.Name = "buttonCreateRequisition";
+            this.buttonCreateRequisition.Size = new System.Drawing.Size(146, 39);
+            this.buttonCreateRequisition.TabIndex = 15;
+            this.buttonCreateRequisition.Text = "Create";
+            this.buttonCreateRequisition.UseVisualStyleBackColor = true;
+            // 
+            // buttonSubmitRequisition
+            // 
+            this.buttonSubmitRequisition.Location = new System.Drawing.Point(350, 106);
+            this.buttonSubmitRequisition.Name = "buttonSubmitRequisition";
+            this.buttonSubmitRequisition.Size = new System.Drawing.Size(146, 39);
+            this.buttonSubmitRequisition.TabIndex = 16;
+            this.buttonSubmitRequisition.Text = "Submit";
+            this.buttonSubmitRequisition.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(516, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 17);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Role";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(516, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 17);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Approver";
+            // 
+            // textApprover
+            // 
+            this.textApprover.Location = new System.Drawing.Point(587, 70);
+            this.textApprover.Name = "textApprover";
+            this.textApprover.Size = new System.Drawing.Size(233, 22);
+            this.textApprover.TabIndex = 20;
+            // 
+            // comboRole
+            // 
+            this.comboRole.FormattingEnabled = true;
+            this.comboRole.Items.AddRange(new object[] {
+            "Developer",
+            "Lead Developer",
+            "Requistions Administrator"});
+            this.comboRole.Location = new System.Drawing.Point(589, 30);
+            this.comboRole.Name = "comboRole";
+            this.comboRole.Size = new System.Drawing.Size(231, 24);
+            this.comboRole.TabIndex = 21;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 164);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(799, 229);
+            this.dataGridView1.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 634);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "NeoExpenses Demo App";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEncryptFile;
+        private System.Windows.Forms.Button buttonDecryptFile;
+        private System.Windows.Forms.Button buttonCreateAsmKeys;
+        private System.Windows.Forms.Button buttonExportPublicKey;
+        private System.Windows.Forms.Button buttonImportPublicKey;
+        private System.Windows.Forms.Button buttonGetPrivateKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBoxExportPrivateKey;
+        private System.Windows.Forms.TextBox textUsername;
+        private System.Windows.Forms.MaskedTextBox maskedPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button buttonSubmitRequisition;
+        private System.Windows.Forms.Button buttonCreateRequisition;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboRole;
+        private System.Windows.Forms.TextBox textApprover;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ EN-us/examples/NPC.mwherman2000.NeoExpenses1/NPC.mwherman2000.NeoExpenses1.TestEncryption/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace NPC.mwherman2000.NeoExpenses1.TestEncryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonEncryptFile = new System.Windows.Forms.Button();
+            this.buttonDecryptFile = new System.Windows.Forms.Button();
+            this.buttonCreateAsmKeys = new System.Windows.Forms.Button();
+            this.buttonExportPublicKey = new System.Windows.Forms.Button();
+            this.buttonImportPublicKey = new System.Windows.Forms.Button();
+            this.buttonGetPrivateKey = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBoxExportPrivateKey = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonEncryptFile
+            // 
+            this.buttonEncryptFile.Location = new System.Drawing.Point(21, 116);
+            this.buttonEncryptFile.Name = "buttonEncryptFile";
+            this.buttonEncryptFile.Size = new System.Drawing.Size(146, 37);
+            this.buttonEncryptFile.TabIndex = 0;
+            this.buttonEncryptFile.Text = "Encrypt File";
+            this.buttonEncryptFile.UseVisualStyleBackColor = true;
+            this.buttonEncryptFile.Click += new System.EventHandler(this.buttonEncryptFile_Click);
+            // 
+            // buttonDecryptFile
+            // 
+            this.buttonDecryptFile.Location = new System.Drawing.Point(211, 21);
+            this.buttonDecryptFile.Name = "buttonDecryptFile";
+            this.buttonDecryptFile.Size = new System.Drawing.Size(146, 42);
+            this.buttonDecryptFile.TabIndex = 1;
+            this.buttonDecryptFile.Text = "Decrypt File";
+            this.buttonDecryptFile.UseVisualStyleBackColor = true;
+            this.buttonDecryptFile.Click += new System.EventHandler(this.buttonDecryptFile_Click);
+            // 
+            // buttonCreateAsmKeys
+            // 
+            this.buttonCreateAsmKeys.Location = new System.Drawing.Point(21, 30);
+            this.buttonCreateAsmKeys.Name = "buttonCreateAsmKeys";
+            this.buttonCreateAsmKeys.Size = new System.Drawing.Size(146, 37);
+            this.buttonCreateAsmKeys.TabIndex = 2;
+            this.buttonCreateAsmKeys.Text = "Create Asm Keys";
+            this.buttonCreateAsmKeys.UseVisualStyleBackColor = true;
+            this.buttonCreateAsmKeys.Click += new System.EventHandler(this.buttonCreateAsmKeys_Click);
+            // 
+            // buttonExportPublicKey
+            // 
+            this.buttonExportPublicKey.Location = new System.Drawing.Point(21, 73);
+            this.buttonExportPublicKey.Name = "buttonExportPublicKey";
+            this.buttonExportPublicKey.Size = new System.Drawing.Size(146, 37);
+            this.buttonExportPublicKey.TabIndex = 3;
+            this.buttonExportPublicKey.Text = "Export Public Key";
+            this.buttonExportPublicKey.UseVisualStyleBackColor = true;
+            this.buttonExportPublicKey.Click += new System.EventHandler(this.buttonExportPublicKey_Click);
+            // 
+            // buttonImportPublicKey
+            // 
+            this.buttonImportPublicKey.Location = new System.Drawing.Point(211, 116);
+            this.buttonImportPublicKey.Name = "buttonImportPublicKey";
+            this.buttonImportPublicKey.Size = new System.Drawing.Size(146, 37);
+            this.buttonImportPublicKey.TabIndex = 4;
+            this.buttonImportPublicKey.Text = "Import Public Keys";
+            this.buttonImportPublicKey.UseVisualStyleBackColor = true;
+            this.buttonImportPublicKey.Click += new System.EventHandler(this.buttonImportPublicKey_Click);
+            // 
+            // buttonGetPrivateKey
+            // 
+            this.buttonGetPrivateKey.Location = new System.Drawing.Point(399, 116);
+            this.buttonGetPrivateKey.Name = "buttonGetPrivateKey";
+            this.buttonGetPrivateKey.Size = new System.Drawing.Size(146, 37);
+            this.buttonGetPrivateKey.TabIndex = 5;
+            this.buttonGetPrivateKey.Text = "Get Private Key";
+            this.buttonGetPrivateKey.UseVisualStyleBackColor = true;
+            this.buttonGetPrivateKey.Click += new System.EventHandler(this.buttonGetPrivateKey_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(208, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "--";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBoxExportPrivateKey);
+            this.groupBox1.Controls.Add(this.buttonExportPublicKey);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.buttonCreateAsmKeys);
+            this.groupBox1.Controls.Add(this.buttonGetPrivateKey);
+            this.groupBox1.Controls.Add(this.buttonEncryptFile);
+            this.groupBox1.Controls.Add(this.buttonImportPublicKey);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(566, 177);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Key Management";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonDecryptFile);
+            this.groupBox2.Location = new System.Drawing.Point(12, 205);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(566, 80);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Decrypt with Current Key";
+            // 
+            // checkBoxExportPrivateKey
+            // 
+            this.checkBoxExportPrivateKey.AutoSize = true;
+            this.checkBoxExportPrivateKey.Checked = true;
+            this.checkBoxExportPrivateKey.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxExportPrivateKey.Location = new System.Drawing.Point(211, 82);
+            this.checkBoxExportPrivateKey.Name = "checkBoxExportPrivateKey";
+            this.checkBoxExportPrivateKey.Size = new System.Drawing.Size(146, 21);
+            this.checkBoxExportPrivateKey.TabIndex = 7;
+            this.checkBoxExportPrivateKey.Text = "Export Private Key";
+            this.checkBoxExportPrivateKey.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 400);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "NeoExpenses Demo App";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEncryptFile;
+        private System.Windows.Forms.Button buttonDecryptFile;
+        private System.Windows.Forms.Button buttonCreateAsmKeys;
+        private System.Windows.Forms.Button buttonExportPublicKey;
+        private System.Windows.Forms.Button buttonImportPublicKey;
+        private System.Windows.Forms.Button buttonGetPrivateKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBoxExportPrivateKey;
+    }
+}
+
